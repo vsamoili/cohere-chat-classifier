1dc7474d33d840e146b612072912bb2e8477720e
vera <vsamoyli@gmail.com>
2024-01-09 16:19:25 +0200

    fixed conversation

--- theydo/cohere_model.py
+++ theydo/cohere_model.py
@@ -7,14 +7,15 @@ import uuid
 import cohere
 from cohere.responses.classify import Example, Classifications
 from cohere.responses.chat import Chat
-from logging import getLogger
+import logging
 
 from theydo import config
 from theydo.data_model import get_training_dir, get_test_dir, Dataset
 from theydo.evaluation import calculate_all
 from theydo.helpers import make_dataset_from_chat_response
 
-logger = getLogger(__name__)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class CohereClassifier:
@@ -48,11 +49,11 @@ class CohereChat:
         self.model = cohere.Client(os.getenv('COHERE_API_KEY'))
         self._clf_model = config['classification_model']
         self._gen_model = config['generation_model']
-        self._gen_temperature = None
+        self._gen_temperature = 0.3     # default temperature
 
         self.chat_delimiter = '###'
         if not base_message:
-            self.chat_base_message = f"""You are a sentiment analysis classifier. You will be provided a list of movie 
+            self.chat_base_message = f"""You are a sentiment analysis classifier. You will be provided a list of 4 movie 
 reviews delimited with '{self.chat_delimiter}'. Classify each movie review as 'positive' or 'negative' based on the 
 sentiment. Provide your output in a json format with two keys: 'text' and 'label' where 'text' is the original review 
 to be classified and 'label' is one of 'positive' or 'negative'. Here is the list:
@@ -72,7 +73,12 @@ to be classified and 'label' is one of 'positive' or 'negative'. Here is the lis
             raise ValueError("Invalid model size specification. Must be one of 'small', 'medium', 'large'.")
 
     def chat(self, msg: str, conversation_id: str) -> Chat:
-        return self.model.chat(message=msg, model=self.gen_model, conversation_id=conversation_id)
+        return self.model.chat(
+            message=msg,
+            model=self.gen_model,
+            conversation_id=conversation_id,
+            temperature=self.gen_temperature
+        )
 
     @property
     def gen_temperature(self):
@@ -84,29 +90,15 @@ to be classified and 'label' is one of 'positive' or 'negative'. Here is the lis
             raise ValueError("Temperature must be a value between 0.0 and 2.0.")
         self._gen_temperature = value
 
-    def initialize_chat(self, conversation_id: str):
-        self.model.chat(message=self.chat_base_message, conversation_id=conversation_id)
-
-    # def create_chat_history(self, previous_prompts: list[str], previous_responses: list[str]) -> list[dict[str, str]]:
-    #     # Append first prompt to previous prompts
-    #     previous_prompts = [{"USER": self.chat_base_message}, *previous_prompts]
-    #
-    #     chat_history = []
-    #     for prompt, response in zip(previous_prompts, previous_responses):
-    #         chat_history.append({"USER": prompt})
-    #         chat_history.append({"CHATBOT": response})
-    #
-    #     return chat_history
-
     def classify_with_prompt(self, inputs: List[str]) -> list[dict[str, str]]:
 
         conversation_id = str(uuid.uuid4())
-        self.initialize_chat(conversation_id=conversation_id)
         all_results = []
 
         i = 0
         while inputs:
 
+            logger.info(f"Classifying {i+4} out of {len(inputs)} reviews...")
             # Iterate through inputs 4 at a time
             inputs = inputs[4*i:4*i+4]
             continuation_prompt = False if i == 0 else True
@@ -127,9 +119,9 @@ to be classified and 'label' is one of 'positive' or 'negative'. Here is the lis
 
     def create_review_prompt(self, texts: List[str], continuation_prompt: bool = False) -> str:
         if continuation_prompt:
-            return f'Do the same for the following reviews: \n{self.chat_delimiter}\n'.join(texts)
+            return f'Do the same for the {len(texts)} following reviews: \n\n{self.chat_delimiter}\n'.join(texts)
         else:
-            return f'\n{self.chat_delimiter}\n'.join(texts)
+            return self.chat_base_message + '\n\n{self.chat_delimiter}\n'.join(texts)
 
     @staticmethod
     def parse_chat_response(response: str) -> Optional[List[dict[str, str]]]:
@@ -160,7 +152,7 @@ if __name__ == "__main__":
     test_set.df = test_set.df.sample(frac=1)
 
     training_set.df = training_set.df.iloc[:100, :]
-    test_set.df = test_set.df.iloc[:3, :]
+    test_set.df = test_set.df.iloc[:20, :]
 
     # complete_message = model.create_complete_prompt(test_set.text_to_list)
     # response = model.chat(complete_message)
